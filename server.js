1dc5af6cc79207dc23b5e5f478db29da75371564
GitRedaAmine <75283738+GitRedaAmine@users.noreply.github.com>
2022-02-12 16:13:21 +0100

    update app

--- server.js
+++ server.js
@@ -8,18 +8,36 @@ const middlewares = jsonServer.defaults({
          logger: true,
 		 noCors  :true
     });
-
+const args = process.argv;
 const port = process.env.PORT || 3001;
 const origineServer= ['https://enima-eshopping.herokuapp.com','http://localhost:4200/','http://127.0.0.1:4200/' ];
+
+let usedIdx=1; 
+
+console.log(args);
+if(args.length >2 ){
+  var prod   = args[2]; //value will be "--prod"
+  console.log(prod);
+  if(prod != "--prod")
+  {
+    usedIdx=1
+    console.log("your arg : " + prod + ' is wrong' );
+  }
+  else
+  {
+    usedIdx=0
+    console.log("your arg : " + prod + ' is ok' );
+  }
+}
+ 
 // /!\ Bind the router db to the app
 server.db = router.db
 
 // Add headers
 server.use(function (req, res, next) {
   'use strict';
-
-  res.header('Access-Control-Allow-Origin', origineServer[0] ); //'https://enima-eshopping.herokuapp.com');
-
+  res.header('Access-Control-Allow-Origin', origineServer[usedIdx] );  
+  console.log( origineServer[usedIdx]   );
   if (req.headers['access-control-request-method']) {
       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   }
@@ -34,6 +52,7 @@ server.use(function (req, res, next) {
       next();
   }
 });
+
 
 
 // You must apply the auth middleware before the router
