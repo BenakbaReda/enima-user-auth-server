d1126b394cc111d9f2d1a817a68302075979b002
GitRedaAmine <75283738+GitRedaAmine@users.noreply.github.com>
2022-02-12 15:23:55 +0100

    update app

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-/*const jsonServer = require('json-server')
-const auth = require('json-server-auth')
-const port = process.env.PORT || 3001;
-
-
-const app = jsonServer.create()
-const router = jsonServer.router('db.json')
-
-// /!\ Bind the router db to the app
-app.db = router.db
-
-// You must apply the auth middleware before the router
-app.use(auth)
-app.use(router)
-app.listen(port,() => {
-  console.log('JSON Server is running')
-})*/
-
-
 const jsonServer = require('json-server');
 const auth = require('json-server-auth')
 
@@ -29,34 +10,15 @@ const middlewares = jsonServer.defaults({
     });
 
 const port = process.env.PORT || 3001;
-const origineServer_Prod= ['https://enima-eshopping.herokuapp.com' ];
+const origineServer= ['https://enima-eshopping.herokuapp.com','http://localhost:4200/','http://127.0.0.1:4200/' ];
 // /!\ Bind the router db to the app
 server.db = router.db
 
 // Add headers
-
- 
-/*
-server.use((req, res, next) => {
-
-  const allowedOrigins = origineServer_Prod;
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
- */
-
-
 server.use(function (req, res, next) {
   'use strict';
 
-  res.header('Access-Control-Allow-Origin', 'https://enima-eshopping.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', origineServer ); //'https://enima-eshopping.herokuapp.com');
 
   if (req.headers['access-control-request-method']) {
       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
